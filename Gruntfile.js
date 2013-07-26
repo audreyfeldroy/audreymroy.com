de912bba0c47ef4c891e0cc5c065c3b1d8712c29
Audrey Roy <audreyr@gmail.com>
2013-07-26 22:41:34 +0200

    Add closureLint task/package.

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,26 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    closureLint: {
+        app:{
+          closureLinterPath : '/Users/audreyr/.virtualenvs/audreymroy.com/lib/python2.7/site-packages/closure_linter',
+          src: [ 'project/assets/js/*' ],
+          options: {
+            stdout: true,
+            strict: true
+          }
+        }
+      },
+      closureFixStyle: {
+        app:{
+          closureLinterPath : '/Users/audreyr/.virtualenvs/audreymroy.com/lib/python2.7/site-packages/closure_linter',
+          src: [ 'project/assets/js/*' ],
+          options: {
+            stdout: true,
+            strict: true
+          }
+        }
+      },
     uglify: {
       options: {
         banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
@@ -14,8 +34,8 @@ module.exports = function(grunt) {
     }
   });
 
-  // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-closure-linter');
 
   // Default task(s).
   grunt.registerTask('default', ['uglify']);
